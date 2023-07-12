44af39288980093cfad1cdb0ac9daf63f8e79edc
zdh <denizh@desird.com>
2023-07-12 09:56:28 +0300

    download from githubusercontent bs removed.

--- install_auto_connect.sh
+++ install_auto_connect.sh
@@ -3,8 +3,10 @@
 echo "What is the APN?"
 read carrierapn
 
-wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_QMI_Installer/master/reconnect_service -O qmi_reconnect.service
-wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_QMI_Installer/master/reconnect_sh -O qmi_reconnect.sh
+cp -f reconnect_service qmi_reconnect.service
+cp -f reconnect_sh qmi_reconnect.sh
+# wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_QMI_Installer/master/reconnect_service -O qmi_reconnect.service
+# wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_QMI_Installer/master/reconnect_sh -O qmi_reconnect.sh
 
 sed -i "s/#APN/$carrierapn/" qmi_reconnect.sh
 

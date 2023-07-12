9b05da25a43f22e07b35e0977f7dfb66376e61f2
zdh <denizh@desird.com>
2023-07-12 10:36:26 +0300

    user input added for service

--- install_auto_connect.sh
+++ install_auto_connect.sh
@@ -3,12 +3,16 @@
 echo "What is the APN?"
 read carrierapn
 
+echo "What is the user?"
+read username
+
 cp -f reconnect_service qmi_reconnect.service
 cp -f reconnect_sh qmi_reconnect.sh
 # wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_QMI_Installer/master/reconnect_service -O qmi_reconnect.service
 # wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_QMI_Installer/master/reconnect_sh -O qmi_reconnect.sh
 
 sed -i "s/#APN/$carrierapn/" qmi_reconnect.sh
+sed -i "s/pi/$username/" qmi_reconnect.service
 
 mv qmi_reconnect.sh /usr/src/
 mv qmi_reconnect.service /etc/systemd/system/
